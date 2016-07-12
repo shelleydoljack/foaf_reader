8329bad5018e5992a6c126152b0a5587f77fa0ef
Shelley Doljack <sdoljack@stanford.edu>
2016-07-12 15:39:06 -0700

    Returns abstracts of interests for people I know.

--- parse_rdf_file.rb
+++ parse_rdf_file.rb
@@ -6,9 +6,26 @@ require 'net/http'
 require 'openssl'
 require 'linkeddata'
 
+# 5. method to get the abstract from a person's interest
+def abstract_for(interest)
+  abs_query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    SELECT ?abs
+      WHERE { ?s dbo:abstract ?abs
+        FILTER (lang(?abs) = 'en')}"
+  abs_graph = RDF::Graph.load(interest)
+  sparql_abstracts_query = SPARQL.parse(abs_query)
+  sparql_abstracts_query.execute(abs_graph) do |result|
+    puts result.abs
+  end
+end
+
+# abstract_for('http://dbpedia.org/resource/Quilting')
+
 graph = RDF::Graph.load("foaf_files/sdoljack_foaf.rdf")
 # graph = RDF::Graph.load("http://stanford.edu/~sdoljack/sdoljack_foaf.rdf")
 
+# after creating an RDF::Graph object, check what type of object it is with .inspect
 puts graph.inspect
 
 # 2. Find everyone I know
@@ -39,6 +56,5 @@ puts "People's interests"
 q_parsed = SPARQL.parse(interests_query)
 q_parsed.execute(graph) do |result|
   puts result.interest
+  abstract_for(result.interest)
 end
-
-# 5.
